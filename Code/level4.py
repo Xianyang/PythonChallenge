3b9adc3e6a9e3df2f0f9f9ffbd739a9dd70a4834
xianyang <xianyang@xianyangs-MacBook-Pro.local>
2016-12-10 13:09:55 +0800

    add note for level4

--- Code/level4.py
+++ Code/level4.py
@@ -1,12 +1,11 @@
-import time
 import urllib2, re
 
 # solution 1 this is my solution
 
 basic_url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
-# next_nothing = basic_nothing = '12345'
+next_nothing = basic_nothing = '12345'
 # next_nothing = basic_nothing = '8022'
-next_nothing = basic_nothing = '63579'
+# next_nothing = basic_nothing = '63579'
 
 while int(next_nothing):
     url = basic_url + next_nothing
@@ -18,7 +17,19 @@ while int(next_nothing):
     else:
         break
 
-print 'end'
-
 # first round: Yes. and the next nothing is 16044, Divide by two and keep going.
+
+# solution 2 - better solution
+import urllib2, re
+basic_url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s'
+next_nothing = '63579'
+
+while True:
+    try:
+        response = urllib2.urlopen(basic_url % next_nothing).read()
+        print response
+        next_nothing = re.search('\d+', response).group(0)
+    except:
+        break
+
 
